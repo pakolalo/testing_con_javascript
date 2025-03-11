ca56be1ae4ce5036a31a5c37c008dbb9d332cb2e
Isco del Collado <fedelcollado@gmail.com>
2025-03-11 14:16:31 -0600

    se agrego 02-math

---
+++ demos/src/02-math.test.js
@@ -0,0 +1,27 @@
+const {sum, multiply, divide} = require('./02-math')
+
+test('add 1 + 3 should be 4', () => {
+  const rta = sum(1,3);
+  expect(rta).toBe(4);
+});
+
+test('should be 4', () => {
+  const rta = multiply(2,2);
+  expect(rta).toBe(4);
+});
+
+test('should divide', () => {
+  const rta = divide(6,3);
+  expect(rta).toBe(2);
+
+  const rta2 = divide(5,2);
+  expect(rta2).toBe(2.5);
+});
+
+test('should divide for 0', () => {
+  const rta = divide(6,0);
+  expect(rta).toBeNull();
+
+  const rta2 = divide(5,0);
+  expect(rta2).toBeNull();
+});
