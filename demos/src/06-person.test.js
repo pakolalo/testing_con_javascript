692ee9c4940f3863dfd6adf4fd7b6ea6377a74b3
Isco del Collado <fedelcollado@gmail.com>
2025-03-13 12:31:14 -0600

    docs(06-person.test.js)add comments [ISCO-250313]

--- demos/src/06-person.test.js
+++ demos/src/06-person.test.js
@@ -2,14 +2,18 @@ const Person = require('./06-person');
 
 describe('Test for Person', () => {
   let person;
-
+  // Arrange / Given
   beforeEach(() => {
     person = new Person('Isco', 45, 1.7);
   });
 
   test('Should return down', () => {
+    // AAA
+    // Arrange / Given
     person.weight = 45;
+    // Act / When
     const imc = person.calcIMC();
+    // Assert / Then
     expect(imc).toBe('down');
   });
 
