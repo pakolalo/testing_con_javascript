2fc4665165417b176d8038ede1c7b351f06d199b
Isco del Collado <fedelcollado@gmail.com>
2025-03-19 14:19:21 -0600

    feat(books.service.test.js)add mock to MongoLib [ISCO250319]

--- api/src/services/books.service.test.js
+++ api/src/services/books.service.test.js
@@ -1,9 +1,28 @@
 const BooksService = require('./books.service');
 
+const fakeBooks = [
+  {
+    _id: 1,
+    name: 'Halo',
+  },
+  {
+    _id: 2,
+    name: 'Gears of War',
+  },
+];
+
+const MongoLibStub = {
+  getAll: () => [...fakeBooks],
+  create: () => {},
+};
+
+jest.mock('../lib/mongo.lib', () => jest.fn().mockImplementation(() => MongoLibStub));
+
 describe('Test for BooksService', () => {
   let service;
   beforeEach(() => {
     service = new BooksService();
+    jest.clearAllMocks();
   });
 
   describe('Test for getBooks', () => {
@@ -14,6 +33,14 @@ describe('Test for BooksService', () => {
       console.log(books);
       // Assert
       expect(books.length).toEqual(2);
+    });
+    test('Should return a list of books', async () => {
+      // Arrange
+      // Act
+      const books = await service.getBooks();
+      console.log(books);
+      // Assert
+      expect(books[0].name).toEqual('Halo');
     });
   });
 });
