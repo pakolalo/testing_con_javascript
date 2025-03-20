7c398fd679a227f3ba1a3ba5f3c3878a77aec10f
Isco del Collado <fedelcollado@gmail.com>
2025-03-19 18:21:42 -0600

    feat(*) add e2e test [ISCO250319]

--- api/e2e/books.e2e.js
+++ api/e2e/books.e2e.js
@@ -1,33 +1,47 @@
-const mockGetAll = jest.fn();
-
 const request = require('supertest');
-
+const { MongoClient } = require('mongodb');
 const createApp = require('../src/app');
+const { config } = require('../src/config');
 
-const { generateManyBooks } = require('../src/fakes/book.fake');
-
-jest.mock('../src/lib/mongo.lib', () => jest.fn().mockImplementation(() => ({
-  getAll: mockGetAll,
-  create: () => {},
-})));
+const DB_NAME = config.dbName;
+const MONGO_URI = config.dbUrl;
 
 describe('Test for books', () => {
   let app = null;
   let server = null;
-  beforeAll(() => {
+  let database = null;
+  beforeAll(async () => {
     app = createApp();
     server = app.listen(3001);
+    const client = new MongoClient(MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    await client.connect();
+    database = client.db(DB_NAME);
   });
 
   afterAll(async () => {
     await server.close();
+    await database.dropDatabase();
   });
 
   describe('test for [GET] /api/v1/books', () => {
-    test('should return a list of books', () => {
+    test('should return a list of books', async () => {
       // Arrange
-      const fakeBooks = generateManyBooks(3);
-      mockGetAll.mockResolvedValue(fakeBooks);
+      const seedData = await database.collection('books').insertMany([
+        {
+          name: 'Book 1',
+          year: 1990,
+          author: 'Isco del Collado',
+        },
+        {
+          name: 'Book 2',
+          year: 2025,
+          author: 'Eduardo Sardaneta',
+        },
+      ]);
+      console.log(seedData);
       // Act
       return request(app)
         .get('/api/v1/books')
@@ -35,7 +49,7 @@ describe('Test for books', () => {
         .then(({ body }) => {
           console.log(body);
           // Assert
-          expect(body.length).toEqual(fakeBooks.length);
+          expect(body.length).toEqual(seedData.insertedCount);
         });
     });
   });
